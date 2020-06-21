7c8d483e833a325e86e755d77aaa3ce2f09b55a2
khizra urooj <Syedakhizra154@gmail.com>
2020-06-21 04:57:46 -0700

    comit completly

---
+++ app.js
@@ -0,0 +1,294 @@
+//Assignment no 6
+//================ Task no 1 ==================//
+//var a = prompt("First Name","Ahmed")
+//var b = prompt("Last Name","Raza")
+//alert(a+b)
+
+//============= Task no 2 ====================//
+
+//var  mob = ("My Favourit pone is samsung galaxy  s6 Edge plus")
+//var b =  mob.length;
+//document.write(b)
+
+//============== Task no 3 ===================//
+//var a = "Pakistani";
+//var b = a.indexOf("n");
+//alert(b)
+
+//============== Task no 4 ===================//
+//var a = "Hello World";
+//var b = a.lastIndexOf("l");
+//alert(b)
+
+//============== Task no 5 ===================//
+//var a = "Pakistani";
+//var b = a.charAt("3");
+//alert(b);
+
+//=============== Task no 6 ==================//
+//var a = "Hania";
+//var b = "Tahir";
+//var c = a.concat(b);
+//alert(c)
+
+//=============== Task no 7 =================//
+//var a = "Lahore";
+//var b = a.replace("Lahore","karachi");
+//alert(b)
+
+//=============== Task no 8 ================//
+//var a = "Ali and Sami are best friend. They play cricket and football together";
+//var b = a.replace(/and/g,"&");
+//alert(b)
+
+//=============== Task no 9 ================//
+//var integerString = "472";
+//var num = Number(integerString );
+//alert(num)
+
+
+//=============== Task no 10 ===============//
+//var a = prompt("peanuts");
+//var b = a.toUpperCase("peanuts");
+//alert(b)
+
+//=============== Task no 11 ================//
+//var a = prompt("javascript");
+//var firstWord = userInput.slice ("0 , 1");
+//firstWord = firstWord.toUpperCase();
+//var word = a.slice( 1 );
+//word = word.toLowerCase();
+//var exact = firstWord + word;
+//alert("Exact ;" + titled);
+
+//================ Task no 12 ================//
+//var number = "35.36";
+//var b = number.toString()
+
+//alert(b)
+
+//================ Task no 13 ================//
+//var UserName = prompt("enter your name");
+//var titlelength = UserName.length; 
+//for(i = 0; i < UserName.length; i++){
+//(i = 0 < UserName.slice(i , i , + 1 ) == "!" ||  UserName.slice(i , i , + 1 ) == "@" ||  UserName.slice(i , i , + 1 ) == "!" ||  )
+//alert("Invalid username");
+//}
+
+//================== Task no 14 =================//
+
+//var array = ["cake","paistry","coconut cookies","patties","cream cake"];
+//var bakistry = prompt("enter what do you like eat");
+//bakistry = bakistry.toLowerCase();
+//var refreshments = array.indexOf(bakistry);
+//if (array.indexOf(bakistry) != -1){
+  //  alert(bakistry + "is availabe at index" + refreshments + "in our bakery");
+
+//}
+//else{
+  //  alert(bakistry +"is not available in our bakery");
+
+//}
+
+//=================== Task no 16 ====================//
+//var ear = ["university of karachi"]
+//var b = ear.split();
+//alert(b)
+
+//================== Task no 17 =====================//
+//var a = prompt("pakistan");
+//var b = a.lastIndexOf("n");
+//alert(b)
+
+//================= Task no 18 ====================//
+//var a ="The quick brown fox jumps over the lazy dog";
+//var b = (a.match(/the/g) || []).length;
+//alert(b)
+
+//================ Second pdf ===================//
+
+//==================== Task no 1 ================//
+//var a = Math.abs(3.6897458)
+//var b = Math.round(3)
+//var c = Math.floor(3)
+//var d = Math.ceil(3.1)
+//alert(a)
+//alert(b)
+//alert(c)
+//alert(d)
+
+//===================== Task no 2 ==================//
+//var number = Math.abs(-2.673)
+//var a = Math.round(-3)
+//var b = Math.floor(-3)
+//var c = Math.ceil (-2)
+//alert(number)
+//alert(a)
+//alert(b)
+//alert(c)
+
+//===================== Task no 3 ===================//
+var a = -4;
+var b = (4)
+//alert(b)
+
+//==================== Task no 4 ===================//
+//var round = "random dice value: ";
+//var b = Math.floor(Math.random() + 4)
+//alert(round + b)
+//var round = "random dice value: ";
+//var b = Math.floor(Math.random() + 6)
+//alert(round + b)
+
+//=================== Task no 5 ==================//
+//var a = Math.floor(Math.random() + 2)
+//var b ="random coin value: Heads";
+//alert(a + b);
+//var c = Math.floor(Math.random() + 1)
+//var d ="random coin value: Heads";
+//alert(c + d);
+
+//==================== Task no 6 ==================//
+//var a ="random number between 1 and 100: ";
+//var b = Math.floor(Math.random() * 1 * 100)
+//alert(a + b)
+
+//=================== Task no 7 ====================//
+//var a = prompt("Enter your weight","50","50kgs","50.2kgs","50.2kilograms");
+//alert(a)
+
+//=================== Task no 8 ====================//
+
+
+//==================== pdf 3 ======================//
+
+//==================== Task no 1 ==================//
+//var date = new Date();
+//alert(date)
+
+//==================== Task no 2 ===================//
+//var a = "Current month:";
+//var d = new Date();
+//var b = d.getMonth();
+//var c = (a+b)
+//alert(c)
+
+//====================== Task no 3 ===================//
+//var para = "Today is";
+//var a = new Date();
+//var b = a.getday();
+//var c = (para + b);
+//alert(para + c)
+
+//==================== Task no 4 =====================//
+//var a = "It's fun day"
+//alert(a)
+
+//==================== Task no 5 ====================//
+//var a = prompt("First Fifteen days of the month","15");
+//if (a === "16") {
+  // alert("Last days of the month.");
+//}
+
+//==================== Task no 6 =====================//
+//var time = new Date();
+//var minetime = time.getDate();
+//var mintes = minetime/(10000*60*60)
+//document.write("current Date:" + time +"<br>")
+//document.write("Elapsed milisecond since january 1, 1970:" + minetime + "<br>" );
+//document.write("Elapsed milisecond since january 1, 1970:" + mintes  + "<br>" )
+
+//====================== Task no 7 =====================//
+//var noon = prompt("whether it's before noon")
+//if (noon === "morning") {
+//alert("Its's AM");
+//}
+//else {
+//alert("Its PM");
+//}
+//===================== Task no 8 ====================//
+//var last = "Later Date:"
+//var a = new Date();
+//a.setMonth(11,31)
+//alert(last + a)
+
+//=================== Task no 9 ====================//
+//var a = new Date("April 25,2020");
+//var b = a.getday();
+//alert(b)
+
+//================= Task no 10 =====================//
+
+
+//==================== Task no 14 ====================//
+//var name = prompt("Enter your name");
+//var bill = prompt("billing month");
+//var total = prompt("Total Units");
+//var charge = +prompt("Charges per Unit");
+//var pay = 180;
+//var netAmount = total * charge;
+//var grossAmount = netAmount +  pay;
+//alert("Payable with in due date :" + newAmount + "<br>");
+//alert("Payable with in due date :" + grossAmount );
+
+//============== Chapter no 35 to 38 =================//
+
+//=============== Task no 1 ====================//
+//function greet(){
+  //alert(new Date)
+//}
+//greet();
+
+//====================== Task no 2 ==================//
+//function greet(firstName,lastName){
+  //alert("Ahmed" + "Raza");
+  
+//}
+//greet();
+
+//====================== Task no 3 ===================//
+//function greet(a,b){
+ //var a = "Khizra"
+ //var b = "Tahir"
+ //alert(a+b)
+//}
+//greet(a+b);
+
+//=================== Task no 4 ======================//
+//function main(num1,app,num2){
+  //if(app ==="+"){
+   //return num1 + num2
+  //}
+  //if(app === "*"){
+  // return num1 * num2
+  //}
+  //else{
+    //return "wrong operater"
+  //}
+//}
+//var list1 = name(5,"+",5);
+//var list2 = name(8,"*",3);
+//var list3 = name (2, "#",10)
+//alert(list1 + "<br>")
+//alert(list2 + "<br>")
+//alert(list3 + "<br>")
+
+//=============== Task no 5 ======================//
+//function greet(){
+  //var a = Math.sqrt(55);
+  //alert(a)
+//}
+//greet()
+
+//================== Task no 6 ======================//
+//function greet(){
+  //var num = 5;
+  //var fact = 1;
+  //for(var i=num; i>=1; i--){
+    //fact = fact *i;
+  //}
+  //alert(fact);
+//}
+//greet()
+
+//=================== Task n0 7 ======================//
